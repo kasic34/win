03ab6e10a220fc66b5b79821324c1a36539babab
kasic34 <162434470+kasic34@users.noreply.github.com>
2025-09-22 15:01:18 +0500

    Add files via upload

---
+++ Новая папка (2)/PiskaBobraFormsApp1/PiskaBobraFormsApp1/Question.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PiskaBobraFormsApp1
+{
+    public class Question
+    {
+        public QuestionType Type { get; set; }
+        public object CorrectAnswer { get; set; }
+
+        public Question(QuestionType type, object correctAnswer)
+        {
+            Type = type;
+            CorrectAnswer = correctAnswer;
+        }
+    }
+
+    public enum QuestionType
+    {
+        NumericPair,
+        Text
+    }
+}
